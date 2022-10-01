1f58ba1e3590869e99991be0476022c86e699621
Leo Ye <ye.leo29266@gmail.com>
2022-10-01 15:36:01 -0400

    will work

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -51,7 +51,7 @@ public class Quadratic extends Topic {
         int a = x[0];
         int b = x[1];
         int c = x[2];
-        double determinant = b * b - 4 * a * c;
+        double determinant = (b * b) - 4 * a * c;
         double temp = 0;
 
         double x1, x2;
@@ -69,7 +69,8 @@ public class Quadratic extends Topic {
             }
             String ans1 = new DecimalFormat("#.##").format(x1);
             String ans2 = new DecimalFormat("#.##").format(x2);
-            answer += ans1 + " and " + ans2;
+            answer += ans1 + ", " + ans2;
+            // System.out.println(answer);
             return answer;
 
         }
@@ -78,6 +79,7 @@ public class Quadratic extends Topic {
             String ans1 = new DecimalFormat("#.##").format(x1);
             String ans2 = new DecimalFormat("#.##").format(x2);
             answer += ans1 + ", " + ans2;
+            // System.out.println(answer);
             return answer;
         }
         else
@@ -181,8 +183,9 @@ public class Quadratic extends Topic {
 
     // testing file
     // public static void main(String[] args) {
-    //    Quadratic test = new Quadratic("testQuestion");
-    //    test.getQuestion();
+    //    int[] nums = {2, -3, 1};
+    //    getQuadraticAnswer(nums);
+    //
     // }
 
 }
